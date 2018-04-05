b5bf1fde40a370b4b75a1c436244b51af7347ffc
Drewster727 <drewm727@hotmail.com>
2018-04-05 12:41:19 -0500

    update version and description

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info < (2, 7):
     install_requires.append('argparse>=1.1')
 
 setup(name='cronwrap',
-      version = '1.3',
+      version = '2.0',
       author="amix the lucky stiff",
       author_email="amix@amix.dk",
       url="http://www.amix.dk/",
@@ -35,16 +35,16 @@ setup(name='cronwrap',
       description="A cron job wrapper that wraps jobs and enables better error reporting and command timeouts.",
       long_description="""\
 Example
--------
+===========
 
 Basic example of usage::
 
-    ##Will print out help
     $ cronwrap -h
 
-        usage: cronwrap [-h] [-c CMD] [-e EMAILS] [-t TIME] [-v [VERBOSE]]
+        usage: cronwrap [-h] [-c CMD] [-e EMAILS] [-t TIME] [-k] [-r] [-v]
 
-        A cron job wrapper that wraps jobs and enables better error reporting and command timeouts.
+        A cron job wrapper that wraps jobs and enables better error reporting and
+        command timeouts. Version 2.0
 
         optional arguments:
           -h, --help            show this help message and exit
@@ -55,27 +55,34 @@ Basic example of usage::
                                 "johndoe@mail.com, marcy@mail.com"`. Uses system's
                                 `mail` to send emails. If no command (cmd) is set a
                                 test email is sent.
-          -t TIME, --time TIME  Set the maxium running time. If this time is passed an
-                                alert email will be sent once the command ends.
-                                The command will keep running even if maximum running time
-                                is exceeded. Please note that this option doesn't
-                                prevent the job from stalling and does nothing to
-                                prevent running multiple cron jobs at the same time.
-                                The default is 1 hour `-t 1h`. Possible values include:
-                                `-t 2h`,`-t 2m`, `-t 30s`.
-          -v [VERBOSE], --verbose [VERBOSE]
-                                Will send an email / print to stdout on successful run.
+          -t TIME, --time TIME  Set the maximum running time.If this time is passed an
+                                alert email will be sent.The command will keep running
+                                even if maximum running time is exceeded.The default
+                                is 1 hour `-t 1h`. Possible values include: `-t
+                                2h`,`-t 2m`, `-t 30s`.
+          -k, --kill            Will kill the process if the maximum running time is
+                                exceeded. This argument will only apply when the -t
+                                (or --time) argument is set.
+          -r, --retry           Retry when the command errors out.
+          -v, --verbose         Will send an email / print to stdout on successful
+                                run.
 
-
-    ##Will send out a timeout alert to cron@my_domain.com:
+    Send out a timeout alert to cron@my_domain.com:
     $ cronwrap -c "sleep 2" -t "1s" -e cron@my_domain.com
+    
+    Send out a timeout alert to cron@my_domain.com and kill the command:
+    $ cronwrap -c "sleep 2" -t "1s" -k -e cron@my_domain.com
 
-    ##Will send out an error alert to cron@my_domain.com:
+    Send out an error alert to cron@my_domain.com:
     $ cronwrap -c "blah" -e cron@my_domain.com
+    
+    Retry a failed command continuously
+    $ cronwrap -c "blah" -r -e cron@my_domain.com
 
-    #Will not send any reports:
+    Don't send any reports:
     $ cronwrap -c "ls" -e cron@my_domain.com
 
-    #Will send a successful report to cron@my_domain.com:
+    Send a successful report to cron@my_domain.com:
     $ cronwrap -c "ls" -e cron@my_domain.com -v
+
 """)
